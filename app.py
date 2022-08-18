d52a4d1b19a0f21fa04d157e57524049c493dc2e
NigerianCoder <gbemilanre@gmail.com>
2022-08-18 10:21:55 +0100

    flask crud completed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from urllib import request
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -18,7 +18,37 @@ from models.models import Book
 def hello():
     return "Hello"
 
-
+# add Book function 
+@app.route('/add', methods=['POST'])
+def add_book():
+    book_data = request.json
+    
+    name = book_data['name']
+    author = book_data['author']
+    published = book_data['published']
+    print(name, author, published)
+    
+    try:
+        book = Book(
+            name=name,
+            author=author,
+            published=published
+        )
+        db.session.add(book)
+        db.session.commit()
+        return "Book added. book id={}".format(book.id)
+    except Exception as e:
+        return (str(e))
+    
+   
+@app.route('/getall')
+def get_all():
+    try:
+        books = Book.query.all()
+        return  jsonify([e.serializer() for e in books])
+    except Exception as e:
+        return (str(e))   
+    
 @app.route("/name/<name>")
 def get_book_name(name):
     return "name : {}".format(name)
@@ -28,6 +58,11 @@ def get_book_details():
     author = request.args.get('author')
     published = request.args.get('published')
     return "Author : {}, Published: {}".format(author, published)
+
+@app.route('/book/<id_>')
+def get_bookid(id_):
+    book_id = Book.query.filter_by(id=id_).first()
+    return jsonify(book_id.serializer())
 
 db.create_all()
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] #+ 'postgresql+psycopg2://emmybook:Book123_@localhost/bookstoredb'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] 
     
     
 class ProductionConfig(Config):
